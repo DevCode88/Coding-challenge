be0f6586a13530b1aae3746f9c61f5474560f0bf
Naveen Suthar <136985202+DevCode88@users.noreply.github.com>
2023-10-14 19:01:46 +0530

    Create Prog_01.java

---
+++ Prog_01.java
@@ -0,0 +1,14 @@
+//Even and Odd
+public class Prog_01 {
+    public static void main(String[] args){
+        int n = 5;
+        int k = 6;
+        n = k;
+        if (n % 2 == 0){
+            System.out.println(n + " is 'Even' number");
+        }
+        else{
+            System.out.println(n + " is 'Odd' number");
+        }
+    }
+}
