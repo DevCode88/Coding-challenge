439a31c73a85da233d61691fda1c87163da5520c
Naveen Suthar <136985202+DevCode88@users.noreply.github.com>
2023-10-21 20:35:43 +0530

    Create  Merge_k_Sorted_Lists.java

---
+++ Java/Merge_k_Sorted_Lists.java
@@ -0,0 +1,42 @@
+class  Merge_k_Sorted_Lists {
+    public ListNode mergeKLists(ListNode[] lists) {
+      int lists = [[1,4,5],[1,3,4],[2,6]];
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeKListsHelper(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        if (start + 1 == end) {
+            return merge(lists[start], lists[end]);
+        }
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        curr.next = (l1 != null) ? l1 : l2;
+        
+        return dummy.next;
+    }
+}
